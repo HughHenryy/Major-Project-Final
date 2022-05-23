142e7dbb7e010b3ef1f4f259c7844237020e6fcd
minhle-work <93075964+minhle-work@users.noreply.github.com>
2022-05-24 00:23:12 +1000

    Update serial_encoder.h
    comments added

--- Major project/Sources/serial_encoder.h
+++ Major project/Sources/serial_encoder.h
@@ -1,6 +1,7 @@
 #ifndef SERIAL_ENCODER_HEADER
 #define SERIAL_ENCODER_HEADER
 
+// struct od the header message
 struct MSG_HEADER{
   int sentinel;
   char msg_type[8];
@@ -9,6 +10,7 @@ struct MSG_HEADER{
   int end_sentinel;
 };
 
+// struct of the data message
 struct MSG_DATA{
   int sentinel;
   int angle;
@@ -17,8 +19,10 @@ struct MSG_DATA{
   unsigned int last_sample_time;
 };
 
+// send text message to the serial port
 void SendTextMsg(char* text_message);
 
+// send data message to the serial port
 void SendDataMsg(int *angle_in, unsigned long *sample_in, unsigned long *base_in);
 
 #endif
