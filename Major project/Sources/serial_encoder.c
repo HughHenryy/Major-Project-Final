971cb7c52fc08770ec2bf28055641054bea5c4af
minhle-work <93075964+minhle-work@users.noreply.github.com>
2022-05-24 00:23:28 +1000

    Update serial_encoder.c
    comments added

--- Major project/Sources/serial_encoder.c
+++ Major project/Sources/serial_encoder.c
@@ -7,17 +7,22 @@
 #include "music_module.h"
 #include "serial_encoder.h"
 
+// send text message to the serial port
 void SendTextMsg(char* text_message) {
+  // set up the struct header to the packed message
   struct MSG_HEADER text_header = {0xABCD, "text", 0, 0, 0xDCBA};
-  text_header.msg_size = strlen(text_message);
-  text_header.header_time = TCNT;
+  text_header.msg_size = strlen(text_message); // get the text message size
+  text_header.header_time = TCNT; // get the time
   
-  SerialOutputBytes((char*)&text_header, sizeof(struct MSG_HEADER), &SCI1);  
-  SerialOutputBytes(text_message, text_header.msg_size, &SCI1);
+  SerialOutputBytes((char*)&text_header, sizeof(struct MSG_HEADER), &SCI1); // output each byte to serial port
+  SerialOutputBytes(text_message, text_header.msg_size, &SCI1); // output each byte to serial port
 }
 
+// send data message to the serial port
 void SendDataMsg(int *angle_in, unsigned long *sample_in, unsigned long *base_in) {
+  // set up the struct header to the packed message
   struct MSG_HEADER data_header = {0xABCD, "DATA", 0, 0, 0xDCBA};
+  // set up the struct message to the packed message
   struct MSG_DATA data_message = {0x9876, 0, 0, 0, 0};
                              
   data_header.msg_size = sizeof(struct MSG_DATA);
@@ -28,6 +33,6 @@ void SendDataMsg(int *angle_in, unsigned long *sample_in, unsigned long *base_in
   data_message.sample = *sample_in;
   data_message.base = *base_in;
   
-  SerialOutputBytes((char*)&data_header, sizeof(struct MSG_HEADER), &SCI1);  
-  SerialOutputBytes((char*)&data_message, sizeof(struct MSG_DATA), &SCI1);  
+  SerialOutputBytes((char*)&data_header, sizeof(struct MSG_HEADER), &SCI1); // output each byte to serial port  
+  SerialOutputBytes((char*)&data_message, sizeof(struct MSG_DATA), &SCI1); // output each byte to serial port 
 }
